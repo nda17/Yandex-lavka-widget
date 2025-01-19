aac8b2cea0913521982c135bf5cdd28c2eb85fed
Dmitry <khv1702@gmail.com>
2025-01-19 20:42:29 +0300

    Test

--- src/js/script.js
+++ src/js/script.js
@@ -16,44 +16,54 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	};
 
-	const dragstart = e => {
-		e.target.classList.add('item-hold');
-		setTimeout(() => e.target.classList.add('item-hide'), 0);
-		activeTarget = e.target;
-	};
-
-	const dragend = e => {
-		activeTarget = null;
-		e.target.classList.remove('item-hold');
-		e.target.classList.remove('item-hide');
-	};
-
-	const dragOver = e => {
+	const dragMove = e => {
 		e.preventDefault();
+
+		let touch = e.targetTouches[0];
+
+		e.target.style.top = `${touch.pageY}px`;
+		e.target.style.left = `${touch.pageX}px`;
+
+		// e.target.classList.add('item-hold');
+		// setTimeout(() => e.target.classList.add('item-hide'), 0);
+		// activeTarget = e.target;
 	};
 
-	const drop = () => {
-		cart.append(activeTarget);
-		activeTarget.classList.add('active');
-		cart.classList.remove('cart-hovered');
-		countProducts < 3 && checkProducts();
-	};
+	// const dragend = e => {
+	// 	activeTarget = null;
+	// 	e.target.classList.remove('item-hold');
+	// 	e.target.classList.remove('item-hide');
+	// };
 
-	const dragEnter = () => {
-		cart.classList.add('cart-hovered');
-	};
+	// const dragOver = e => {
+	// 	e.preventDefault();
+	// };
 
-	const dragLeave = () => {
-		cart.classList.remove('cart-hovered');
-	};
+	// const drop = () => {
+	// 	cart.append(activeTarget);
+	// 	activeTarget.classList.add('active');
+	// 	cart.classList.remove('cart-hovered');
+	// 	countProducts < 3 && checkProducts();
+	// };
+
+	// const dragEnter = e => {
+	// 	cart.classList.add('cart-hovered');
+	// };
+
+	// const dragLeave = () => {
+	// 	cart.classList.remove('cart-hovered');
+	// };
 
 	storeShelf.forEach(item => {
-		item.addEventListener('dragstart', dragstart);
-		item.addEventListener('dragend', dragend);
+		item.addEventListener('touchmove', dragMove);
+		// item.addEventListener('touchend', drop);
+		// touchstart
+		// item.addEventListener('dragend', dragend);
+		// touchend
 	});
 
-	cart.addEventListener('dragover', dragOver);
-	cart.addEventListener('drop', drop);
-	cart.addEventListener('dragenter', dragEnter);
-	cart.addEventListener('dragleave', dragLeave);
+	// cart.addEventListener('dragover', dragOver);
+	// item.addEventListener('touchend', drop);
+	// cart.addEventListener('dragenter', dragEnter);
+	// cart.addEventListener('dragleave', dragLeave);
 });
