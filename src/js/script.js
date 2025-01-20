884cc77537fe735f674fc47b4b862948593ab337
Dmitry <khv1702@gmail.com>
2025-01-20 05:09:42 +0300

    Added logic

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,19 @@
 window.addEventListener('DOMContentLoaded', () => {
+	const container = document.querySelector('.banner-container');
 	const storeShelf = document.querySelectorAll('.item');
 	const cart = document.querySelector('.cart');
 	const paymentBtn = document.querySelector('.wrapper-payment-btn');
 	let activeTarget = null;
 	let touchEvent = null;
 	let countProducts = 0;
+
+	const defaultStateProduct = () => {
+		const itemName = activeTarget.classList[0];
+		document.querySelector(`.${itemName}`).style.top = '';
+		document.querySelector(`.${itemName}`).style.left = '';
+		touchEvent = null;
+		activeTarget = null;
+	};
 
 	const checkProducts = () => {
 		countProducts = countProducts + 1;
@@ -17,45 +26,40 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	};
 
-	const dragTouch = e => {
+	const dragTouchMove = e => {
 		e.preventDefault();
 		activeTarget = e.target;
 		touchEvent = e.targetTouches[0];
 
-		let touch = e.targetTouches[0];
-
-		e.target.style.top = `${touch.pageY}px`;
-		e.target.style.left = `${touch.pageX}px`;
+		e.target.style.top = `${
+			touchEvent.pageY -
+			container.offsetTop -
+			e.target.offsetHeight / 2 -
+			28
+		}px`;
+		e.target.style.left = `${
+			touchEvent.pageX -
+			container.offsetLeft -
+			e.target.offsetWidth / 2 -
+			44.32
+		}px`;
 	};
 
-	// const dragend = e => {
-	// 	activeTarget = null;
-	// 	e.target.classList.remove('item-hold');
-	// 	e.target.classList.remove('item-hide');
-	// };
-
-	// const dragOver = e => {
-	// 	e.preventDefault();
-	// };
-
 	const dropTouch = e => {
-		if (touchEvent.pageY > 330) {
+		if (touchEvent.pageY > 360) {
 			cart.append(activeTarget);
 			activeTarget.classList.add('active');
 			touchEvent = null;
 			activeTarget = null;
 			countProducts < 3 && checkProducts();
 		} else {
+			defaultStateProduct();
 		}
 	};
 
 	storeShelf.forEach(item => {
-		item.addEventListener('touchmove', dragTouch);
+		item.addEventListener('touchstart', dropTouchStart);
+		item.addEventListener('touchmove', dragTouchMove);
 		item.addEventListener('touchend', dropTouch);
 	});
-
-	// cart.addEventListener('dragover', dragOver);
-	// cart.addEventListener('touchend', () => console.log('fffff'));
-	// cart.addEventListener('dragenter', dragEnter);
-	// cart.addEventListener('dragleave', dragLeave);
 });
